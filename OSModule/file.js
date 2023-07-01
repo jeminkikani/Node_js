4bb6ed372e25fdae6f3ffe94172fe79b84cf590f
Jemin <skilljeminkikani9510@gmail.com>
2023-07-01 18:03:58 +0530

    01-07-23

---
+++ OSModule/file.js
@@ -0,0 +1,26 @@
+const os = require('os');
+
+// console.log(os.arch());
+// console.log(os.EOL);
+// console.log(os.homedir());
+// console.log(os.networkInterfaces());
+// console.log(os.version());
+// console.log(os.type());
+// console.log(os.freemem());
+// console.log(os.totalmem());
+// console.log(os.userInfo());
+// console.log(os.availableParallelism());
+// console.log(os.constants);
+// console.log(os.userInfo());
+// console.log(os.cpus());
+// console.log(os.devNull);
+// console.log(os.getPriority(0));
+// console.log(os.hostname());
+// console.log(os.loadavg());
+// console.log(os.machine());
+// console.log(os.platform());
+// console.log(os.release());
+// console.log(os.tmpdir());
+console.log(os.uptime());
+     
+
